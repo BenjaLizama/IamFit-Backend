7280dc8fe7df718650aec843fea3a534b17de8c9
BenjaLizama <be.lizamac@duocuc.cl>
2026-06-29 21:53:22 -0400

    test: Implementa pruebas para usuarios-perfiles

--- usuarios-perfiles/src/test/java/com/iamfit/usuarios_perfiles/UsuariosPerfilesApplicationTests.java
+++ usuarios-perfiles/src/test/java/com/iamfit/usuarios_perfiles/UsuariosPerfilesApplicationTests.java
@@ -2,14 +2,21 @@ package com.iamfit.usuarios_perfiles;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @ActiveProfiles("test")
 class UsuariosPerfilesApplicationTests {
 
+	// Simula el JwtDecoder para que el contexto no falle intentando leer la llave pública
+	@MockitoBean
+	private JwtDecoder jwtDecoder;
+
 	@Test
 	void contextLoads() {
+		// Si el test pasa, significa que el contexto arrancó correctamente
 	}
 
 }
